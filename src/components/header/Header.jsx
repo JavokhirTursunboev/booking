dfa93117bb45445a4af3266a8703c1067f29f562
Javokhir <jtursunboev@bk.ru>
2023-09-11 21:37:05 +0900

    header is done

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,49 @@
+import {
+  faBed,
+  faCar,
+  faPlane,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import "./header.css";
+function Header() {
+  return (
+    <div className="header">
+      <div className="headerContainer ">
+        {/* header List part */}
+        <div className="headerList">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attraction</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport Taxis</span>
+          </div>
+        </div>
+
+        {/* Header Title part */}
+        <h1 className="headerTitle">Find your next stay</h1>
+        <p className="headerDesc">
+          Search deals on hotels, homes, and much more..
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.css";
 function Home() {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   );
 }
