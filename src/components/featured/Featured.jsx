7eed72e133c61a7614e0ff70d7c087feef099983
Javokhir <jtursunboev@bk.ru>
2023-09-15 20:52:43 +0900

    property is done

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import './featured.css'
+function Featured() {
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <img
+          src="https://xx.bstatic.com/xdata/images/xphoto/1182x887/244344247.jpg?k=8c4f62a42b3c7feb649489d3eaa779ffc1e8b063e7a9b891dd013a76c9497708&o=?size=M"
+          alt=""
+          className="featuredImage"
+        />
+        <div className="featuredTitle">
+          <h1>Dublin</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://xx.bstatic.com/xdata/images/xphoto/1182x887/244344247.jpg?k=8c4f62a42b3c7feb649489d3eaa779ffc1e8b063e7a9b891dd013a76c9497708&o=?size=M"
+          alt=""
+          className="featuredImage"
+        />
+        <div className="featuredTitle">
+          <h1>Dublin</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://xx.bstatic.com/xdata/images/xphoto/1182x887/244344247.jpg?k=8c4f62a42b3c7feb649489d3eaa779ffc1e8b063e7a9b891dd013a76c9497708&o=?size=M"
+          alt=""
+          className="featuredImage"
+        />
+        <div className="featuredTitle">
+          <h1>Dublin</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Featured;
